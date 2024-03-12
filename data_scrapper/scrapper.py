c15eab2c2959f5f73d2ba366a3c3d644fd77b0c9
alditus <alvarez.aldoandres@gmail.com>
2024-03-12 11:15:16 -0300

    extract all tags 'a' that contains the url corresponding to a specific letter in guarani or spanish

--- data_scrapper/scrapper.py
+++ data_scrapper/scrapper.py
@@ -1,5 +1,6 @@
 import requests
 import pathlib
+import re
 from bs4 import BeautifulSoup
 from settings import ORIGIN_URL, SOURCE_HTML_NAME
 
@@ -12,17 +13,62 @@ def get_origin_source_response(origin_url):
 	request_result = requests.get(origin_url)
 	return request_result.text
 
-def save_result_to_disk(text,where_to_save):
+def save_result_to_disk(text, where_to_save):
 	with open(where_to_save, mode='w', encoding='UTF-8') as file:
 		file.write(text)
 
+def find_all_tags_of_interest(soup):
+	"""
+	We are interested to extract the url from the 'a' tag corresponding to the following block of code in source.html
+
+	<li class="first">
+		<span class="item-title"><a href="/2012/index.php/diccionario-guarani/599-guarani-espanol/tai-a">
+			Tai A</a>
+		</span>
+		<dl><dt>
+			Article Count:</dt>
+			<dd>1</dd>
+		</dl>
+		
+	</li>
+
+	"""
+	result = soup.find_all(filter_li)
+	print(result)
+	for r in result:
+		print(r)
+
+def filter_li(tag, language_prefix):
+	"""
+	This function return the li tag we are interested
+	"""
+	return tag.name == 'a' and tag.has_attr('href') and tag['href'] != '#' and tag['href'] != '' and tag.string and language_prefix in tag.string.lower()
+
 
 if __name__ == '__main__':
+	source_text = ''
 	# check whether we already downloaded origin source
 	if pathlib.Path(SOURCE_HTML_NAME).is_file():
-		print(f'{SOURCE_HTML_NAME} already downloaded proceed to next steps')
+		print(f'{SOURCE_HTML_NAME} already downloaded, proceed to next steps')
 	else:
 		print(f'Downloading html source from {ORIGIN_URL} and saving it to local file {SOURCE_HTML_NAME}')
 		response_text = get_origin_source_response(ORIGIN_URL)
-		save_result_to_disk(response_text,where_to_save=SOURCE_HTML_NAME)
+		save_result_to_disk(response_text, where_to_save=SOURCE_HTML_NAME)
+
+
+	# at this point, we should already have our source.html downloaded so let's load it and open it with bs4
+	with open(SOURCE_HTML_NAME, mode='r', encoding='UTF-8') as file:
+		source_text = file.read()
+
+	# parse it with beautiful soup
+	soup = BeautifulSoup(source_text, 'html.parser')
+	
+	all_a_gn = soup.find_all(lambda x: filter_li(x, 'tai '))
+	all_a_sp = soup.find_all(lambda x: filter_li(x, 'letra '))
+
+
+	print(all_a_gn+all_a_sp)
+
+
+
 
--- data_scrapper/settings.py
+++ data_scrapper/settings.py
@@ -1,2 +1,3 @@
 ORIGIN_URL = "http://descubrircorrientes.com.ar/2012/index.php/diccionario-guarani"
+ORIGIN_ROOT = "http://descubrircorrientes.com.ar/"
 SOURCE_HTML_NAME = 'source.html'
